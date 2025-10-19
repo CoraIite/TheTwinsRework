e21d4bcf9e606b72d49c2d9c0e853bc65e1a95ad
CoraIite <793708493@qq.com>
2025-10-19 23:49:05 +0800

    Update Beastfly.cs

--- Items/Beastfly.cs
+++ Items/Beastfly.cs
@@ -32,7 +32,6 @@ namespace TheTwinsRework.Items
 
         public override bool CanUseItem(Player player)
         {
-            return false;
             return !NPC.AnyNPCs(ModContent.NPCType<BeastlyQueenBee>());
         }
 
@@ -74,9 +73,9 @@ namespace TheTwinsRework.Items
 
         public override void AddRecipes()
         {
-            //CreateRecipe()
-            //    .AddIngredient(ItemID.Wood, 15)
-            //    .Register();
+            CreateRecipe()
+                .AddIngredient(ItemID.Wood, 15)
+                .Register();
         }
     }
 }
